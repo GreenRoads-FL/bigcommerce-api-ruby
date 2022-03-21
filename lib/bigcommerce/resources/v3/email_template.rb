6fb6a7f08049ce568a10278a95367273178df102
william1benn <william.benn11@gmail.com>
2022-03-21 19:25:36 -0400

    template endpoint

---
+++ lib/bigcommerce/resources/v3/email_template.rb
@@ -0,0 +1,13 @@
+module Bigcommerce
+  module V3
+    class EmailTemplate < Resource
+      include Bigcommerce::ResourceActions.new uri: 'marketing/email-templates/%s'
+
+      property :type_id
+      property :subject
+      property :body
+      property :translations
+      property :meta
+    end
+  end
+end
